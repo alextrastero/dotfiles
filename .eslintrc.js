6cb47b3a81631368de10e94a69a9d048a5864492
Alexander Odell <alextrastero@gmail.com>
2016-05-24 13:05:31 +0200

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "plugins": [
+    "react"
+  ],
+  "extends": [ "eslint:recommended", "plugin:react/recommended" ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [ "error", 2 ],
+    "linebreak-style": [ "error", "unix" ],
+    "quotes": [ "error", "single" ],
+    "comma-dangle": [ "error", "never" ],
+    "semi": [ "error", "always" ],
+    "object-curly-spacing": [ "error", "always" ],
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-uses-vars": 2,
+    "react/jsx-pascal-case": [ 2, "always" ],
+    "react/sort-prop-types": [ 2 , { "requiredFirst": true } ]
+  },
+  "globals": {
+    "React": true
+  }
+};

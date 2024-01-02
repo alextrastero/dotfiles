9793d150b7564ede7ed4bc5e0ba11c5b15b7fd28
Alexander Odell <alextrastero@gmail.com>
2024-01-02 10:19:49 +0100

    finish setup

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,48 +1,53 @@
 return {
   'goolord/alpha-nvim',
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
+    local alpha = require('alpha')
+    local dashboard = require('alpha.themes.dashboard')
     -- require 'alpha'.setup(require'alpha.themes.dashboard'.config)
-local dashboard = require('alpha.themes.dashboard')
--- Set header
-dashboard.section.header.val = {
-  "⠀⠀⠀⢠⣾⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⣰⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⢰⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⣀⣀⣤⣤⣶⣾⣿⣿⣿⡷ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠤⠖⠒⠲⣤⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁ ⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣴⣞⣁⠀⠀⠀⠀⠀⠀⠆",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀ ⠀⠀⠀⠀⣠⣶⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀ ⠀⠀⢀⣾⣿⣿⠟⢋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀ ⠀⢀⣾⡿⠛⢁⣴⣿⣿⠋⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⡆⠀⠀",
-  "⣿⣿⣿⡇⠀⡾⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀ ⠀⣼⠟⢀⣴⣿⣿⣿⣇⣀⣀⣀⣀⣀⣘⣿⣿⣿⣿⣿⣿⣿⠀⠀",
-  "⣿⣿⣿⣧⡀⠁⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ ⠀⠁⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢹⠉⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ ⠀⢠⣾⣿⣿⣿⣿⣿⡏⠉⠉⠉⠉⠉⠉⣉⣉⣉⣉⣉⣉⣉⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⣀⣼⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀ ⢀⣿⣿⣿⣿⣿⣿⣿⣿⣄⡀⠀⠀⣀⣼⣿⣿⣿⣿⣿⣿⡟⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀ ⣼⣿⠏⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⡟⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀",
-  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠀⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⣧⠀⠀⠀⠀⠀⢈⡩⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀",
-  "⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⠿⠋⢃⠈⠢⡁⠒⠄⡀⠈⠁⠀⠀⠀⠀⠀⠀⠀ ⠈⠛⠿⠶⠖⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⣿⣿⠟⠁⠀⠀⠈⠉⠉⠁⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "",
-  "chore: add Oyster build script",
-  "docs: explain hat wobble",
-  "feat: add beta sequence",
-  "fix: remove broken confirmation message",
-  "refactor: share logic between 4d3d3d3 and flarhgunnstow",
-  "style: convert tabs to spaces",
-  "test: ensure Tayne retains clothing"
-}
+    -- Set header
+    dashboard.section.header.val = {
+      "⠀⠀⠀⢠⣾⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⣰⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⢰⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⣀⣀⣤⣤⣶⣾⣿⣿⣿⡷ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠤⠖⠒⠲⣤⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁ ⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣴⣞⣁⠀⠀⠀⠀⠀⠀⠆",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀ ⠀⠀⠀⠀⣠⣶⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀ ⠀⠀⢀⣾⣿⣿⠟⢋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀ ⠀⢀⣾⡿⠛⢁⣴⣿⣿⠋⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⡆⠀⠀",
+      "⣿⣿⣿⡇⠀⡾⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀ ⠀⣼⠟⢀⣴⣿⣿⣿⣇⣀⣀⣀⣀⣀⣘⣿⣿⣿⣿⣿⣿⣿⠀⠀",
+      "⣿⣿⣿⣧⡀⠁⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ ⠀⠁⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢹⠉⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ ⠀⢠⣾⣿⣿⣿⣿⣿⡏⠉⠉⠉⠉⠉⠉⣉⣉⣉⣉⣉⣉⣉⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⣀⣼⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀ ⢀⣿⣿⣿⣿⣿⣿⣿⣿⣄⡀⠀⠀⣀⣼⣿⣿⣿⣿⣿⣿⡟⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀ ⣼⣿⠏⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⡟⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀",
+      "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠀⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⣧⠀⠀⠀⠀⠀⢈⡩⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀",
+      "⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⠿⠋⢃⠈⠢⡁⠒⠄⡀⠈⠁⠀⠀⠀⠀⠀⠀⠀ ⠈⠛⠿⠶⠖⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⣿⣿⠟⠁⠀⠀⠈⠉⠉⠁⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "",
+      "chore: add Oyster build script",
+      "docs: explain hat wobble",
+      "feat: add beta sequence",
+      "fix: remove broken confirmation message",
+      "refactor: share logic between 4d3d3d3 and flarhgunnstow",
+      "style: convert tabs to spaces",
+      "test: ensure Tayne retains clothing"
+    }
 
--- Set menu
-dashboard.section.buttons.val = {
-  dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("f", "Find file", ":cd $HOME/dev | Telescope find_files<CR>"),
-  dashboard.button("r", "Recent", ":Telescope oldfiles<CR>"),
-  dashboard.button("s", "Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-  dashboard.button("q", "Quit NVIM", ":qa<CR>"),
-}
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "Find file", ":cd $HOME/dev | Telescope find_files<CR>"),
+      dashboard.button("r", "Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "Quit NVIM", ":qa<CR>"),
+    }
 
+    alpha.setup(dashboard.opts)
   end
 }
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -32,77 +32,118 @@ local kind_icons = {
 return {
   {
     'L3MON4D3/LuaSnip',
+    lazy = false,
     run = 'make install_jsregexp',
     dependencies = {
       'rafamadriz/friendly-snippets',
       'saadparwaiz1/cmp_luasnip'
+    },
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    branch = 'main',
+    opts = {
+      preview = {
+        lines_above = 2,
+        lines_below = 10,
+      },
+      scroll_preview = {
+        scroll_down = "<C-f>",
+        scroll_up = "<C-b>",
+      },
+      request_timeout = 2000,
+      lightbulb = {
+        enable = false,
+      },
+      definition = {
+        width = 1,
+        height = 1,
+      },
+      implement = {
+        virtual_text = false,
+      },
+      finder = {
+        layout = "normal",
+      }
     }
   },
-  'hrsh7th/nvim-cmp',
-  dependencies = {
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lua',
+  {
     'hrsh7th/cmp-nvim-lsp',
+    lazy = false,
+    config = true,
   },
-  opts = function()
-    local cmp = require('cmp')
-    require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    'hrsh7th/nvim-cmp',
+    lazy = false,
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lua',
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      local cmp = require('cmp')
 
-    return {
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      formatting = {
-        format = function(entry, vim_item)
-          vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-          vim_item.menu = ({
-            buffer = "[buf]",
-            nvim_lsp = "[lsp]",
-            nvim_lua = "[api]",
-            path = "[path]",
-            luasnip = "[snip]",
-          })[entry.source.name]
-          return vim_item
-        end
-      },
-      mapping = {
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<Tab>'] = function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+            vim_item.menu = ({
+              buffer = "[buf]",
+              nvim_lsp = "[lsp]",
+              nvim_lua = "[api]",
+              path = "[path]",
+              luasnip = "[snip]",
+            })[entry.source.name]
+            return vim_item
           end
-        end,
-        ['<S-Tab>'] = function(fb)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fb()
-          end
-        end,
-      },
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        -- { name = "buffer" },
-        -- { name = "nvim_lua" },
-      },
-    }
-  end
+        },
+        mapping = {
+          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+          ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+          ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+          ['<S-Tab>'] = function(fb)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fb()
+            end
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "luasnip" },
+          { name = "nvim_lsp" },
+            -- { name = "nvim_lsp" },
+            -- { name = "luasnip" },
+          -- },
+          -- {
+            -- { name = "buffer" },
+            -- { name = "nvim_lua" },
+          }
+        ),
+      })
+    end
+  }
 }

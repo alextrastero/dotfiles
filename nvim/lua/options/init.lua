81b00e0a6f4c4651a70c29ca36eb8d9f006d1739
Alexander Odell <alextrastero@gmail.com>
2024-01-03 18:12:36 +0100

    fix colors

--- nvim/lua/options/init.lua
+++ nvim/lua/options/init.lua
@@ -35,30 +35,3 @@ opt.list = true
 opt.listchars = 'trail:?'     -- trailing spaces
 opt.listchars = opt.listchars + 'tab:\\ \\'    --tabs
 opt.diffopt = opt.diffopt + 'vertical'
-
--- COLORS
-vim.cmd [[set background=dark]]
---vim.cmd [[colorscheme catppuccin-macchiato]]
-vim.cmd [[hi clear LspReferenceRead]] -- this is not working...
-vim.cmd [[hi link LspReferenceRead IncSearch]]
-vim.cmd [[set cursorline]]
-
--- vim.cmd [[hi clear CursorLineNR]]
-vim.cmd [[hi CursorLine guibg=NONE guifg=NONE]]
--- vim.cmd [[hi link CursorLineNR Question]]
-
-vim.cmd [[hi Comment gui=italic]]
-vim.cmd [[hi clear NvimTreeNormal]]
-vim.cmd [[hi link NvimTreeNormal Normal]]
--- vim.cmd [[hi clear SignColumn]]
-vim.cmd [[hi! link SignColumn LineNr]]
-
--- Highlight opened folder in nvim-tree
-vim.cmd [[hi NvimTreeOpenedFolderName gui=underline]]
--- hi NvimTreeFolderName
-
-vim.cmd [[hi clear Search]]
-vim.cmd [[hi! link Search Visual]]
-
-vim.cmd [[hi link NvimTreeIndentMarker VertSplit]]
-
--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -5,5 +5,31 @@ return {
   name = "catppuccin",
   config = function()
     vim.cmd [[colorscheme catppuccin-macchiato]]
+
+    -- COLORS
+    vim.cmd [[set background=dark]]
+    --vim.cmd [[colorscheme catppuccin-macchiato]]
+    vim.cmd [[hi clear LspReferenceRead]] -- this is not working...
+    vim.cmd [[hi link LspReferenceRead IncSearch]]
+    vim.cmd [[set cursorline]]
+
+    -- vim.cmd [[hi clear CursorLineNR]]
+    vim.cmd [[hi CursorLine guibg=NONE guifg=NONE]]
+    -- vim.cmd [[hi link CursorLineNR Question]]
+
+    vim.cmd [[hi Comment gui=italic]]
+    vim.cmd [[hi clear NvimTreeNormal]]
+    vim.cmd [[hi link NvimTreeNormal Normal]]
+    -- vim.cmd [[hi clear SignColumn]]
+    vim.cmd [[hi! link SignColumn LineNr]]
+
+    -- Highlight opened folder in nvim-tree
+    vim.cmd [[hi NvimTreeOpenedFolderName gui=underline]]
+    -- hi NvimTreeFolderName
+
+    vim.cmd [[hi clear Search]]
+    vim.cmd [[hi! link Search Visual]]
+
+    vim.cmd [[hi link NvimTreeIndentMarker VertSplit]]
   end
 }

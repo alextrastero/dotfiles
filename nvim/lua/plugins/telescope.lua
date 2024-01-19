5a25e0413d9f279e49851065e1f8234442275454
Alexander Odell <alextrastero@gmail.com>
2024-01-19 15:33:15 +0100

    Revert "add telescope-ui lib"
    This reverts commit 384d53a74b2192ba93b79db4b43eaa75000d4887.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,43 +1,26 @@
 return {
-  {
-    -- https://github.com/nvim-telescope/telescope-ui-select.nvim
-    'nvim-telescope/telescope-ui-select.nvim'
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
+  'nvim-telescope/telescope.nvim',
+  opts = {
+    defaults = {
+      dynamic_preview_title = true,
+      path_display = { "truncate" },
+    },
+    pickers = {
+      find_files = {
+        -- theme = "dropdown",
+        hidden = true,
+        layout_config = {
+          width = 0.99,
+          preview_cutoff = 0.99,
         },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-    opts = {
-      defaults = {
-        dynamic_preview_title = true,
-        path_display = { "truncate" },
+        -- layout_config = {
+        --   width = 0.99,
+        -- },
       },
-      pickers = {
-        find_files = {
-          -- theme = "dropdown",
-          hidden = true,
-          layout_config = {
-            width = 0.99,
-            preview_cutoff = 0.99,
-          },
-          -- layout_config = {
-          --   width = 0.99,
-          -- },
-        },
-        buffers = {
-          sort_lastused = true,
-          sort_mru = true,
-          ignore_current_buffer = true,
-        },
+      buffers = {
+        sort_lastused = true,
+        sort_mru = true,
+        ignore_current_buffer = true,
       },
     },
   },

6662f18627407055af1a57a2c6d9c1142a2bd304
Alexander Odell <alextrastero@gmail.com>
2024-02-05 13:10:44 +0100

    enable git blame

--- nvim/lua/options/init.lua
+++ nvim/lua/options/init.lua
@@ -66,8 +66,8 @@ vim.cmd[[
 -- git blame
 -- Available options: <author>, <committer>, <date>, <committer-date>, <summary>, <sha>
 -- vim.g.gitblame_message_template = '<summary> • <date> • <author>'
-vim.g.gitblame_display_virtual_text = 0
--- vim.g.gitblame_enabled = 0
+-- vim.g.gitblame_display_virtual_text = 0
+vim.g.gitblame_enabled = 0
 -- vim.g.gitblame_delay = 1000 -- 1 second
 -- vim.g.gitblame_highlight_group = "Question"
 

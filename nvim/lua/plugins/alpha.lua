6b6274d87c7e6d6b7675521462c505f6019fda3b
Alexander Odell <alextrastero@gmail.com>
2024-02-16 10:51:17 +0100

    update alpha

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -5,36 +5,31 @@ return {
     local dashboard = require('alpha.themes.dashboard')
 
     dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[     chore: add Oyster build script                                    ]],
-      [[      docs: explain hat wobble                                         ]],
-      [[       feat: add beta sequence                                         ]],
-      [[        fix: remove broken confirmation message                        ]],
-      [[         refactor: share logic between 4d3d3d3 and flarhgunnstow       ]],
-      [[          style: convert tabs to spaces                                ]],
-      [[           test: ensure Tayne retains clothing                         ]]
+      [[                    ________ ++     ________                ]],
+      [[                   /VVVVVVVV\++++  /VVVVVVVV\               ]],
+      [[                   \VVVVVVVV/++++++\VVVVVVVV/               ]],
+      [[                    |VVVVVV|++++++++/VVVVV/'                ]],
+      [[                    |VVVVVV|++++++/VVVVV/'                  ]],
+      [[                   +|VVVVVV|++++/VVVVV/'+                   ]],
+      [[                 +++|VVVVVV|++/VVVVV/'+++++                 ]],
+      [[               +++++|VVVVVV|/VVVVV/'+++++++++               ]],
+      [[                 +++|VVVVVVVVVVV/'+++++++++                 ]],
+      [[                   +|VVVVVVVVV/'+++++++++                   ]],
+      [[                    |VVVVVVV/'+++++++++                     ]],
+      [[                    |VVVVV/'+++++++++                       ]],
+      [[                    |VVV/'+++++++++                         ]],
+      [[                    'V/'   ++++++                           ]],
+      [[                             ++                             ]],
+      [[                                                            ]],
+      [[                                                            ]],
+      [[      [chore, docs, feat, fix, refactor, style, test]       ]],
     }
-
-    -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "Find file", ":cd $HOME/dev | Telescope find_files<CR>"),
+      dashboard.button("e", "New", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "Find", ":cd $HOME/dev | Telescope find_files<CR>"),
       dashboard.button("r", "Recent", ":Telescope oldfiles<CR>"),
       dashboard.button("s", "Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-      dashboard.button("q", "Quit NVIM", ":qa<CR>"),
+      dashboard.button("q", "Quit", ":qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)

16c09c1c0d12a725a0861ada90813b3808ccc8c7
Alexander Odell <alextrastero@gmail.com>
2024-02-26 23:11:55 +0100

    add eslint code actions

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,8 +9,9 @@ return {
       sources = {
         require('none-ls.diagnostics.eslint_d'),
         require('none-ls.formatting.eslint_d'),
+        require('none-ls.code_actions.eslint_d'),
         null_ls.builtins.diagnostics.stylelint,
-        null_ls.builtins.formatting.stylelint,
+        -- null_ls.builtins.formatting.stylelint,
       },
     })
   end,

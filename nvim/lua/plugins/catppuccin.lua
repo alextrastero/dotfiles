5d889568abc61f9261804d1b9e745e100ea67eb4
Alexander Odell <alextrastero@gmail.com>
2024-04-01 15:30:56 +0200

    remove unused config

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -10,20 +10,8 @@ return {
     vim.cmd [[hi clear LspReferenceRead]] -- this is not working...
     vim.cmd [[hi link LspReferenceRead IncSearch]]
 
-    -- vim.cmd [[set cursorline]]
-    -- vim.cmd [[hi clear CursorLineNR]]
-    -- vim.cmd [[hi CursorLine guibg=NONE guifg=NONE]]
-    -- vim.cmd [[hi link CursorLineNR Question]]
-
     vim.cmd [[hi Comment gui=italic]]
-    vim.cmd [[hi clear NvimTreeNormal]]
-    vim.cmd [[hi link NvimTreeNormal Normal]]
-    -- vim.cmd [[hi clear SignColumn]]
     vim.cmd [[hi! link SignColumn LineNr]]
-
-    -- Highlight opened folder in nvim-tree
-    vim.cmd [[hi NvimTreeOpenedFolderName gui=underline]]
-    -- hi NvimTreeFolderName
 
     vim.cmd [[hi clear Search]]
     vim.cmd [[hi! link Search Visual]]

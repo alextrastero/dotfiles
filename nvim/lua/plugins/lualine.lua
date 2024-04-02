cdf7bfbe92df37a5eebde413457921f245b6064e
Alexander Odell <alextrastero@gmail.com>
2024-04-02 12:06:32 +0200

    remove lsp-progress

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,22 +2,11 @@ local function sessionName()
   return 'mk:' .. require('auto-session.lib').current_session_name()
 end
 
--- local lsp_progress = require('lsp-progress')
--- lsp_progress.setup({
---   -- Spinning icons.
---   spinner = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
---   -- Spinning update time in milliseconds.
---   spin_update_time = 200,
--- })
-
 local git_blame = require('gitblame')
 
 return {
   'nvim-lualine/lualine.nvim',
   event = { "VimEnter" },
-  dependencies = {
-    -- 'linrongbin16/lsp-progress.nvim',
-  },
   opts = {
     options = {
       component_separators = { left = '|', right = '|' },
@@ -36,7 +25,6 @@ return {
           git_blame.get_current_blame_text,
           cond = git_blame.is_blame_text_available,
         },
-        -- require('lsp-progress').progress,
       },
     },
     tabline = {

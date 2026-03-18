77ca997bee65bd916a087972f2c9c4770b57ce2b
Alexander Odell <alextrastero@gmail.com>
2026-03-18 14:45:10 +0100

    add everforest theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,9 +4,20 @@ return {
   { "maxmx03/solarized.nvim" },
   { "rebelot/kanagawa.nvim" },
   {
+    "neanias/everforest-nvim",
+    config = function()
+      require("everforest").setup({
+        background = "soft",
+      })
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      -- colorscheme = "catppuccin-latte",
+      colorscheme = "everforest",
     },
   },
 }

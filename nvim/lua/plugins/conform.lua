c53a4237a8cd696cf2a9fab60be5584db90fb1e2
Alexander Odell <alextrastero@gmail.com>
2026-04-21 15:51:48 +0200

    update to conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,19 +1,57 @@
-local jsFormaters = { "eslint_d", "prettier", stop_after_first = true }
+local eslint_configs = {
+  ".eslintrc",
+  ".eslintrc.js",
+  ".eslintrc.cjs",
+  ".eslintrc.yaml",
+  ".eslintrc.yml",
+  ".eslintrc.json",
+  "eslint.config.js",
+  "eslint.config.mjs",
+  "eslint.config.cjs",
+  "eslint.config.ts",
+  "eslint.config.mts",
+}
+
+local function has_eslint()
+  local root = vim.fn.getcwd()
+  for _, name in ipairs(eslint_configs) do
+    if vim.uv.fs_stat(root .. "/" .. name) then
+      return true
+    end
+  end
+  -- also check for eslintConfig key in package.json
+  local pkg = root .. "/package.json"
+  if vim.uv.fs_stat(pkg) then
+    local ok, data = pcall(vim.fn.readfile, pkg)
+    if ok then
+      local content = table.concat(data, "\n")
+      if content:find('"eslintConfig"') then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+local function js_formatters()
+  if has_eslint() then
+    return { "prettier" }
+  else
+    return { "oxfmt" }
+  end
+end
 
 return {
   "stevearc/conform.nvim",
-  opts = function()
-    local u = require("conform.util")
-    return {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = jsFormaters,
-        javascriptreact = jsFormaters,
-        typescript = jsFormaters,
-        typescriptreact = jsFormaters,
-        jsonc = { "prettier" },
-        ["*"] = { "trim_whitespace" },
-      },
-    }
-  end,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = js_formatters,
+      javascriptreact = js_formatters,
+      typescript = js_formatters,
+      typescriptreact = js_formatters,
+      jsonc = { "prettier" },
+      ["*"] = { "trim_whitespace" },
+    },
+  },
 }

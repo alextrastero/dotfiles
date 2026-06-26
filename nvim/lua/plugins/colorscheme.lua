aa0554eefc5fb287ceeb167e430dc9669cc3a3f1
Alexander Odell <alextrastero@gmail.com>
2026-06-26 10:00:43 +0200

    enable auto theme switching with OS

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,18 @@ return {
     end,
   },
 
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.o.background = "dark"
+      end,
+      set_light_mode = function()
+        vim.o.background = "light"
+      end,
+    },
+  },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",

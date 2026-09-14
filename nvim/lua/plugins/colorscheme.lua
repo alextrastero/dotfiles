4efc9624b808d5f83d91472ba4d9d6d9127609ce
Alexander Odell <alextrastero@gmail.com>
2026-09-14 12:14:12 +0200

    remove auto dark functionality

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,18 +12,6 @@ return {
     end,
   },
 
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      set_dark_mode = function()
-        vim.o.background = "dark"
-      end,
-      set_light_mode = function()
-        vim.o.background = "light"
-      end,
-    },
-  },
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",

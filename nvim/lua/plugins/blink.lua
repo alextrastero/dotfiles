ff61b8010b5cfa7b98301a0116059e5a9e0c3096
Alexander Odell <alextrastero@gmail.com>
2026-09-23 12:00:11 +0200

    remove unused blink lua methods

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,29 +1,3 @@
---- Checks wether node under cursor is of type `reject`
---- @param _reject string[]
---- @return boolean
-local isDisabled = function(_reject)
-  -- Get cursor position
-  local row, column = unpack(vim.api.nvim_win_get_cursor(0))
-
-  -- Check if we're inside a comment or string
-  local success, node = pcall(vim.treesitter.get_node, {
-    pos = { row - 1, math.max(0, column - 1) },
-    ignore_injections = false,
-  })
-
-  -- DEBUG which node type is undercursor
-  -- print("Node Type:", success and node and node:type() or "No Node")
-  return not not (success and node and vim.tbl_contains(_reject, node:type()))
-end
-
-local isComment = function()
-  return isDisabled({
-    "comment",
-    "line_comment",
-    "comment_content",
-  })
-end
-
 return {
   {
     "saghen/blink.cmp",
@@ -31,8 +5,6 @@ return {
     ---@type blink.cmp.Config
     opts = {
       enabled = function()
-        -- if isDisabled() then
-        --   return false
         -- -- **Disable Blink in LSP rename popup**
         if vim.bo.buftype == "nofile" then
           return true
@@ -121,8 +93,6 @@ return {
         default = function()
           if vim.bo.filetype == "markdown" then
             return { "lsp", "path" }
-          elseif isComment() then
-            return { "buffer" }
           else
             -- Default sources for all file types
             return { "buffer", "lsp", "snippets", "path" }
